bd6f50b0ff6d6fc868acc48ac6defbc922702d83
kungran <kungrankang@gmail.com>
2024-03-22 21:11:15 +0900

    no message

---
+++ js/apply.js
@@ -0,0 +1,195 @@
+//min
+
+// ran
+
+// hun
+
+$(document).ready(function () {
+  //min
+  function getCurrentDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+
+    // 월이나 일이 한 자릿수인 경우 앞에 0을 붙임
+    month = month < 10 ? "0" + month : month;
+    day = day < 10 ? "0" + day : day;
+
+    return `${year}-${month}-${day}`;
+  }
+
+  // 페이지 로드 시 현재 날짜를 input에 설정
+  $("#date").val(getCurrentDate());
+  // 현재 날짜 이전의 날짜 선택 불가능하도록 설정
+  $("#date").attr("min", getCurrentDate());
+  //hun
+  var area0 = ["서울특별시", "인천광역시", "대전광역시", "광주광역시", "대구광역시", "울산광역시", "부산광역시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주도"];
+  var areas = [
+    [
+      "강남구",
+      "강동구",
+      "강북구",
+      "강서구",
+      "관악구",
+      "광진구",
+      "구로구",
+      "금천구",
+      "노원구",
+      "도봉구",
+      "동대문구",
+      "동작구",
+      "마포구",
+      "서대문구",
+      "서초구",
+      "성동구",
+      "성북구",
+      "송파구",
+      "양천구",
+      "영등포구",
+      "용산구",
+      "은평구",
+      "종로구",
+      "중구",
+      "중랑구",
+    ],
+    ["계양구", "남구", "남동구", "동구", "부평구", "서구", "연수구", "중구", "강화군", "옹진군"],
+    ["대덕구", "동구", "서구", "유성구", "중구"],
+    ["광산구", "남구", "동구", "북구", "서구"],
+    ["남구", "달서구", "동구", "북구", "서구", "수성구", "중구", "달성군"],
+    ["남구", "동구", "북구", "중구", "울주군"],
+    ["강서구", "금정구", "남구", "동구", "동래구", "부산진구", "북구", "사상구", "사하구", "서구", "수영구", "연제구", "영도구", "중구", "해운대구", "기장군"],
+    [
+      "고양시",
+      "과천시",
+      "광명시",
+      "광주시",
+      "구리시",
+      "군포시",
+      "김포시",
+      "남양주시",
+      "동두천시",
+      "부천시",
+      "성남시",
+      "수원시",
+      "시흥시",
+      "안산시",
+      "안성시",
+      "안양시",
+      "양주시",
+      "오산시",
+      "용인시",
+      "의왕시",
+      "의정부시",
+      "이천시",
+      "파주시",
+      "평택시",
+      "포천시",
+      "하남시",
+      "화성시",
+      "가평군",
+      "양평군",
+      "여주군",
+      "연천군",
+    ],
+    ["강릉시", "동해시", "삼척시", "속초시", "원주시", "춘천시", "태백시", "고성군", "양구군", "양양군", "영월군", "인제군", "정선군", "철원군", "평창군", "홍천군", "화천군", "횡성군"],
+    ["제천시", "청주시", "충주시", "괴산군", "단양군", "보은군", "영동군", "옥천군", "음성군", "증평군", "진천군", "청원군"],
+    ["계룡시", "공주시", "논산시", "보령시", "서산시", "아산시", "천안시", "금산군", "당진군", "부여군", "서천군", "연기군", "예산군", "청양군", "태안군", "홍성군"],
+    ["군산시", "김제시", "남원시", "익산시", "전주시", "정읍시", "고창군", "무주군", "부안군", "순창군", "완주군", "임실군", "장수군", "진안군"],
+    ["광양시", "나주시", "목포시", "순천시", "여수시", "강진군", "고흥군", "곡성군", "구례군", "담양군", "무안군", "보성군", "신안군", "영광군", "영암군", "완도군", "장성군", "장흥군", "진도군", "함평군", "해남군", "화순군"],
+    ["경산시", "경주시", "구미시", "김천시", "문경시", "상주시", "안동시", "영주시", "영천시", "포항시", "고령군", "군위군", "봉화군", "성주군", "영덕군", "영양군", "예천군", "울릉군", "울진군", "의성군", "청도군", "청송군", "칠곡군"],
+    ["거제시", "김해시", "마산시", "밀양시", "사천시", "양산시", "진주시", "진해시", "창원시", "통영시", "거창군", "고성군", "남해군", "산청군", "의령군", "창녕군", "하동군", "함안군", "함양군", "합천군"],
+    ["서귀포시", "제주시", "남제주군", "북제주군"],
+  ];
+
+ // Populate the 시/도 dropdown
+ $("#sido").append(area0.map(function(area) {
+  return $("<option>").val(area).text(area);
+}));
+
+// 시/도 선택 change event
+$("#sido").change(function() {
+  var index = $("#sido option").index($("#sido option:selected")) - 1; // Adjusted for zero-indexing and the placeholder option
+  var selectedArea = areas[index] || [];
+  var $gugun = $("#gugun").empty().append("<option value=''>구/군 선택</option>");
+
+  $.each(selectedArea, function() {
+    $gugun.append($("<option>").val(this).text(this));
+  });
+});
+$('.js-click-modal').click(function(){
+  $('.container').addClass('modal-open');
+});
+
+$('.js-close-modal').click(function(){
+  $('.container').removeClass('modal-open');
+});
+function openModal() {
+  document.querySelector(".app-modal_wrap").style.display = "block";
+}
+
+// 모달 닫기 함수
+function closeModal() {
+  document.querySelector(".app-modal_wrap").style.display = "none";
+}
+$('#app-bt').prop('disabled', true);
+// 신청하기 버튼 클릭 시 모달 열기
+document.getElementById("app-bt").addEventListener("click", function (event) {
+  event.preventDefault(); // 폼 제출 이벤트 방지
+  openModal();
+});
+
+// // 모달 외부 클릭 시 모달 닫기 이벤트 추가
+// document.querySelector(".app-modal_wrap").addEventListener("click", function (event) {
+//   if (event.target.classList.contains(".app-modal_wrap")) {
+//     closeModal();
+//   }
+// });
+
+let seconds; // 남은 시간 변수
+let countdown; // 카운트다운을 관리하는 변수
+const $timeSpan = $('.time'); // 시간을 표시할 요소
+const $btnSend = $('.code-bt'); // "인증번호 받기" 버튼 요소
+const $btnAuth = $('.btn_auth') // "인증 하기" 버튼 요소
+
+$('#btn_auth').prop('disabled', true);
+
+// 시간을 업데이트하고 화면에 표시하는 함수
+const updateCountdown = function() {
+    if (seconds >= 0) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        $timeSpan.text(`${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`);
+        seconds--;
+    } else {
+        clearInterval(countdown);
+        alert('인증번호 유효시간이 만료되었습니다.');
+    }
+};
+
+// "인증번호 받기" 버튼 클릭 이벤트 핸들러
+$btnSend.on('click', function(e) {
+    e.preventDefault();
+    $btnSend.text('재전송');
+    alert('인증번호가 발송되었습니다.');
+
+    $('#btn_auth').prop('disabled', false);
+
+    clearInterval(countdown);
+    seconds = 180; // 3분(180초)
+
+    updateCountdown();
+    // 1초마다 카운트다운 업데이트
+    countdown = setInterval(updateCountdown, 1000); 
+});
+$btnAuth.on('click', function(e) {
+  e.preventDefault();
+  $btnSend.text('인증 요청');
+  alert('인증이 완료되었습니다.');
+
+  $timeSpan.hide();
+  // 신청 하기 버튼 활성화
+  $('#app-bt').prop('disabled', false);
+});
+});
+
---
+++ js/main.js
@@ -0,0 +1,455 @@
+window.onload = function () {
+  // 모달창
+  // 모달을 숨기는 함수
+  function hideModal() {
+    document.getElementById("popup").style.display = "none";
+  }
+
+  // // "다시 보지 않기" 버튼 이벤트 리스너
+  // document.getElementById("neverShowAgain").addEventListener("click", function () {
+  //   // localStorage에 사용자의 선택 저장
+  //   localStorage.setItem("hideModal", "true");
+  //   hideModal();
+  // });
+
+  // // 페이지 로드 시 모달 상태 확인
+  // window.onload = function () {
+  //   // 사용자가 "다시 보지 않기"를 선택했는지 확인
+  //   if (localStorage.getItem("hideModal") === "true") {
+  //     hideModal();
+  //   }
+  // };
+
+  // // "닫기" 버튼 이벤트 리스너 (선택적 구현)
+  // document.getElementById("close").addEventListener("click", function () {
+  //   hideModal();
+  // });
+
+  //min
+  //menu area
+  //topmenu
+  // index 페이지의 메뉴 항목을 찾아서 해당 항목에 active 클래스를 추가합니다.
+  const currentPage = "index.html"; // 현재 페이지의 URL을 설정합니다.
+  const topMenu = document.querySelector(".top-menu");
+  const topMenuLi = document.querySelectorAll(".top-menu > li");
+
+  topMenuLi.forEach((li) => {
+    const link = li.querySelector("a"); // 각 li 요소의 하위 a 요소를 찾습니다.
+    const href = link.getAttribute("href"); // a 요소의 href 속성 값을 가져옵니다.
+
+    // 만약 현재 페이지의 URL이 해당 li 요소의 href 속성 값과 일치한다면 active 클래스를 추가합니다.
+    if (href === currentPage) {
+      li.classList.add("active");
+    }
+  });
+
+  topMenu.addEventListener("mouseleave", () => {
+    topMenuLi.forEach((item) => {
+      const link = item.querySelector("a");
+      const href = link.getAttribute("href");
+      if (href === currentPage) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+    });
+  });
+
+  topMenuLi.forEach((li) => {
+    li.addEventListener("click", () => {
+      // 클릭된 li에만 active 클래스를 추가하고 다른 li에서는 제거합니다.
+      topMenuLi.forEach((item) => {
+        if (item === li) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    });
+
+    li.addEventListener("mouseover", () => {
+      // 호버한 메뉴 항목에 .active 클래스를 추가하고 이전에 추가된 클래스를 제거합니다.
+      topMenuLi.forEach((item) => {
+        if (item === li) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    });
+  });
+  //top menu scroll logo hidden
+  const topLogo = document.querySelector(".logo");
+  const langBox = document.querySelector(".lang");
+  let scy = 0;
+
+  window.addEventListener("scroll", function () {
+    // 새로 고침 / url 입력해서 html 출력시
+    // 1.스크롤바의 픽셀 위치값을 파악해서
+    scy = this.document.documentElement.scrollTop;
+    // 2.class 적용 여부 결정
+    if (scy > 0) {
+      topLogo.classList.add("logoh");
+      langBox.classList.add("logoh");
+    } else {
+      topLogo.classList.remove("logoh");
+      langBox.classList.remove("logoh");
+    }
+  });
+  //language
+  const langToggle = document.querySelector(".lang-more");
+  const langM = document.querySelector(".lang > li:last-of-type");
+  langToggle.addEventListener("click", () => {
+    langM.classList.toggle("show");
+  });
+  langM.addEventListener("click", () => {
+    langM.classList.remove("show");
+  });
+  //480 point logo img chg
+  const fnLogoChg = document.querySelector(".logo > a > img");
+  const innerWidth = window.innerWidth;
+  if (innerWidth <= 480) {
+    fnLogoChg.src = "images/favicon/apple-icon-76x76.png";
+  }
+  //ani product link
+  var fnScNavLinkElement = document.querySelector(".ani-product a");
+  fnScNavLinkElement.addEventListener("click", function (e) {
+    e.preventDefault();
+    var targetSectionId = this.getAttribute("href");
+    var targetSection = document.querySelector(targetSectionId);
+    var targetPosition = targetSection.offsetTop;
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
+  });
+  var fnAniImgChg = document.querySelector(".ani-img");
+  var fnAniArea = document.querySelector(".ani-product");
+
+  fnAniArea.addEventListener("mouseenter", function () {
+    fnAniImgChg.src = "images/ani-product-h-01.png";
+  });
+
+  fnAniArea.addEventListener("mouseleave", function () {
+    fnAniImgChg.src = "images/ani-product.png";
+  });
+  //quiz area
+  const fnAnswerBox = document.querySelectorAll(".answer-box .answer-box-li");
+  const fnChoiceBox = document.querySelectorAll(".choice-box .choice-box-li");
+  const fnChkAnswerSub = document.querySelector(".answer-sub");
+  let currentIdx = 0;
+  let animationPlayed = false;
+
+  // Click event handler for choice boxes
+  function clickHandler(box) {
+    if (currentIdx < fnChoiceBox.length) {
+      fnAnswerBox[currentIdx].textContent = box.textContent;
+      fnAnswerBox[currentIdx].classList.add("jsAnswer");
+      currentIdx++;
+      // 모든 fnAnswerBox가 채워졌는지 확인하고 정답 확인 및 버튼 활성화/비활성화
+      const isAllFilled = Array.from(fnAnswerBox).every((li) => li.textContent.trim().length > 0);
+      if (isAllFilled) {
+        fnChkAnswerSub.classList.add("active-as");
+      }
+    }
+  }
+
+  // Add click event listener to choice boxes
+  fnChoiceBox.forEach((box) => {
+    box.addEventListener("click", () => {
+      clickHandler(box);
+    });
+  });
+
+  fnChkAnswerSub.addEventListener("click", () => {
+    const userAnswer = Array.from(fnAnswerBox)
+      .map((box) => box.textContent)
+      .join(""); //join() 배열의 모든 요소를 하나의 문자열로 공백없이 결합
+    const collecAnswer = "파티마겔";
+    if (userAnswer == collecAnswer && !animationPlayed) {
+      document.querySelector(".qbg2").classList.add("qbg2V");
+      animationPlayed = true;
+      //party-popper
+      const TWO_PI = Math.PI * 2;
+      const HALF_PI = Math.PI * 0.5;
+
+      // canvas settings
+      var viewWidth = 512,
+        viewHeight = 350,
+        drawingCanvas = document.getElementById("drawing_canvas"),
+        ctx,
+        timeStep = 1 / 40;
+
+      Point = function (x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+      };
+
+      Particle = function (p0, p1, p2, p3) {
+        this.p0 = p0;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+
+        this.time = 0;
+        this.duration = 3 + Math.random() * 2;
+        this.color = "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+
+        this.w = 8;
+        this.h = 6;
+
+        this.complete = false;
+      };
+
+      Particle.prototype = {
+        update: function () {
+          this.time = Math.min(this.duration, this.time + timeStep);
+
+          var f = Ease.outCubic(this.time, 0, 1, this.duration);
+          var p = cubeBezier(this.p0, this.p1, this.p2, this.p3, f);
+
+          var dx = p.x - this.x;
+          var dy = p.y - this.y;
+
+          this.r = Math.atan2(dy, dx) + HALF_PI;
+          this.sy = Math.sin(Math.PI * f * 10);
+          this.x = p.x;
+          this.y = p.y;
+
+          this.complete = this.time === this.duration;
+        },
+        draw: function () {
+          ctx.save();
+          ctx.translate(this.x, this.y);
+          ctx.rotate(this.r);
+          ctx.scale(1, this.sy);
+
+          ctx.fillStyle = this.color;
+          ctx.fillRect(-this.w * 0.5, -this.h * 0.5, this.w, this.h);
+
+          ctx.restore();
+        },
+      };
+
+      Loader = function (x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.r = 24;
+        this._progress = 0;
+
+        this.complete = false;
+      };
+
+      Loader.prototype = {
+        reset: function () {
+          this._progress = 0;
+          this.complete = false;
+        },
+        set progress(p) {
+          this._progress = p < 0 ? 0 : p > 1 ? 1 : p;
+
+          this.complete = this._progress === 1;
+        },
+        get progress() {
+          return this._progress;
+        },
+        draw: function () {
+          ctx.fillStyle = "#fff";
+          ctx.beginPath();
+          ctx.arc(this.x, this.y, this.r, -HALF_PI, TWO_PI * this._progress - HALF_PI);
+          ctx.lineTo(this.x, this.y);
+          ctx.closePath();
+          ctx.fill();
+        },
+      };
+
+      // pun intended
+      Exploader = function (x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.startRadius = 24;
+
+        this.time = 0;
+        this.duration = 0.4;
+        this.progress = 0;
+
+        this.complete = false;
+      };
+
+      Exploader.prototype = {
+        reset: function () {
+          this.time = 0;
+          this.progress = 0;
+          this.complete = false;
+        },
+        update: function () {
+          this.time = Math.min(this.duration, this.time + timeStep);
+          this.progress = Ease.inBack(this.time, 0, 1, this.duration);
+
+          this.complete = this.time === this.duration;
+        },
+        draw: function () {
+          ctx.fillStyle = "#fff";
+          ctx.beginPath();
+          ctx.arc(this.x, this.y, this.startRadius * (1 - this.progress), 0, TWO_PI);
+          ctx.fill();
+        },
+      };
+
+      var particles = [],
+        loader,
+        exploader,
+        phase = 0;
+
+      function initDrawingCanvas() {
+        drawingCanvas.width = viewWidth;
+        drawingCanvas.height = viewHeight;
+        ctx = drawingCanvas.getContext("2d");
+
+        createLoader();
+        createExploader();
+        createParticles();
+      }
+
+      function createLoader() {
+        loader = new Loader(viewWidth * 0.5, viewHeight * 0.5);
+      }
+
+      function createExploader() {
+        exploader = new Exploader(viewWidth * 0.5, viewHeight * 0.5);
+      }
+
+      function createParticles() {
+        for (var i = 0; i < 128; i++) {
+          var p0 = new Point(viewWidth * 0.5, viewHeight * 0.5);
+          var p1 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
+          var p2 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
+          var p3 = new Point(Math.random() * viewWidth, viewHeight + 64);
+
+          particles.push(new Particle(p0, p1, p2, p3));
+        }
+      }
+
+      function update() {
+        switch (phase) {
+          case 0:
+            loader.progress += 1 / 45;
+            break;
+          case 1:
+            exploader.update();
+            break;
+          case 2:
+            particles.forEach(function (p) {
+              p.update();
+            });
+            break;
+        }
+      }
+
+      function draw() {
+        ctx.clearRect(0, 0, viewWidth, viewHeight);
+
+        switch (phase) {
+          case 0:
+            loader.draw();
+            break;
+          case 1:
+            exploader.draw();
+            break;
+          case 2:
+            particles.forEach(function (p) {
+              p.draw();
+            });
+            break;
+        }
+      }
+
+      function loop() {
+        update();
+        draw();
+
+        if (phase === 0 && loader.complete) {
+          phase = 1;
+        } else if (phase === 1 && exploader.complete) {
+          phase = 2;
+        } else if (phase === 2 && checkParticlesComplete()) {
+          // reset
+          phase = 0;
+          loader.reset();
+          exploader.reset();
+          particles.length = 0;
+          createParticles();
+        }
+
+        requestAnimationFrame(loop);
+      }
+
+      function checkParticlesComplete() {
+        for (var i = 0; i < particles.length; i++) {
+          if (particles[i].complete === false) return false;
+        }
+        return true;
+      }
+
+      // math and stuff
+
+      /**
+       * easing equations from http://gizma.com/easing/
+       * t = current time
+       * b = start value
+       * c = delta value
+       * d = duration
+       */
+      var Ease = {
+        inCubic: function (t, b, c, d) {
+          t /= d;
+          return c * t * t * t + b;
+        },
+        outCubic: function (t, b, c, d) {
+          t /= d;
+          t--;
+          return c * (t * t * t + 1) + b;
+        },
+        inOutCubic: function (t, b, c, d) {
+          t /= d / 2;
+          if (t < 1) return (c / 2) * t * t * t + b;
+          t -= 2;
+          return (c / 2) * (t * t * t + 2) + b;
+        },
+        inBack: function (t, b, c, d, s) {
+          s = s || 1.70158;
+          return c * (t /= d) * t * ((s + 1) * t - s) + b;
+        },
+      };
+
+      function cubeBezier(p0, c0, c1, p1, t) {
+        var p = new Point();
+        var nt = 1 - t;
+
+        p.x = nt * nt * nt * p0.x + 3 * nt * nt * t * c0.x + 3 * nt * t * t * c1.x + t * t * t * p1.x;
+        p.y = nt * nt * nt * p0.y + 3 * nt * nt * t * c0.y + 3 * nt * t * t * c1.y + t * t * t * p1.y;
+
+        return p;
+      }
+      initDrawingCanvas();
+      requestAnimationFrame(loop);
+    } else {
+      document.querySelector(".qbg3").classList.add("show-qbg3");
+      document.querySelector(".quiz-retry-btn").addEventListener("click", function () {
+        document.querySelector(".qbg3").classList.remove("show-qbg3");
+        // 틀렸을 경우 정답 박스 초기화
+        currentIdx = 0;
+        fnAnswerBox.forEach((box) => {
+          box.textContent = "";
+          box.classList.remove("jsAnswer");
+        });
+      });
+    }
+  });
+
+  
+  // hun
+};
